9d2e3084df6af566ed9a54f0a1d6bcda531d4d24
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-04-30 19:37:43 +0200

    add Header
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

---
+++ src/components/Header/Header.view.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react'
+import {
+    Text,
+    Container,
+    ActionIcon,
+    Group,
+    Box,
+    Grid,
+    Center,
+} from '@mantine/core';
+import {IconProps, Icon} from '@tabler/icons-react';
+import Logo from '@components/Logo';
+import * as classes from './Header.module.css';
+
+
+type Props = {
+    socials: {
+        Icon: React.ForwardRefExoticComponent<Omit<IconProps, "ref"> & React.RefAttributes<Icon>>;
+        link: string;
+    }[];
+    description: string;
+}
+
+
+export default function HeaderView({description, socials}: Props): React.JSX.Element {
+    return <Box component="header" className={classes.header}>
+        <Container fluid mx="xl" px="xl" py={{
+            base: 0,
+            xs: 'xs',
+        }}>
+            <Grid justify="space-between" align="center" gutter={0}>
+                <Grid.Col span={4} visibleFrom="xs">
+                    <Group justify="flex-start">
+                        <Text c="gray" size="sm">
+                            {description}
+                        </Text>
+                    </Group>
+                </Grid.Col>
+                <Grid.Col span={{
+                    base: 12,
+                    xs: 4,
+                }}>
+                    <Center>
+                        <Logo alt="G-Research"
+                              src="https://github.com/G-Research/brand/raw/main/logo/GR-OSS/logo.svg"
+                              height={96}/>
+                    </Center>
+                </Grid.Col>
+                <Grid.Col span={4} visibleFrom="xs">
+                    <Group gap={4} justify="flex-end">
+                        {socials?.filter(v => v?.link).map((v, ind) => (
+                            <ActionIcon
+                                size="xl"
+                                color="gray" variant="subtle"
+                                component="a"
+                                href={v.link}
+                                target="_blank" rel="noopener noreferrer"
+                                key={ind}>
+                                <v.Icon size={20}/>
+                            </ActionIcon>
+                        ))}
+                    </Group>
+                </Grid.Col>
+            </Grid>
+        </Container>
+    </Box>
+}
