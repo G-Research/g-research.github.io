b36432b784fffa2b5a659068e4c7983b396ceda1
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-10 08:27:51 +0200

    add core/types
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

---
+++ src/core/types/index.ts
@@ -0,0 +1,16 @@
+export type DataProps = {
+    site: {
+        siteMetadata: {
+            title: string,
+            description: string,
+            websiteUrl: string,
+            blogUrl: string,
+            careersUrl: string,
+            githubUrl: string,
+            xUrl: string,
+            youtubeUrl: string,
+            joinUsUrl: string,
+            generatedAt: Date,
+        }
+    }
+}
