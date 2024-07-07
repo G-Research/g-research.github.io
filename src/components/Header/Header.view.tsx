5e1be500c89dbf45c7b0b81b530c371d630a8934
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-07-07 08:03:57 +0200

    Header: add tooltip to social icons
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- src/components/Header/Header.view.tsx
+++ src/components/Header/Header.view.tsx
@@ -7,6 +7,7 @@ import {
     Box,
     Grid,
     Center,
+    Tooltip,
 } from '@mantine/core';
 import {IconProps, Icon} from '@tabler/icons-react';
 import Logo from '@components/Logo';
@@ -14,15 +15,16 @@ import * as classes from './Header.module.css';
 
 
 type Props = {
+    title: string;
     socials: {
         Icon: React.ForwardRefExoticComponent<Omit<IconProps, "ref"> & React.RefAttributes<Icon>>;
         link: string;
+        label: string;
     }[];
-    description: string;
 }
 
 
-export default function HeaderView({description, socials}: Props): React.JSX.Element {
+export default function HeaderView({title, socials}: Props): React.JSX.Element {
     return <Box component="header" className={classes.header}>
         <Container fluid mx="xl" px="xl" py={{
             base: 0,
@@ -31,8 +33,8 @@ export default function HeaderView({description, socials}: Props): React.JSX.Ele
             <Grid justify="space-between" align="center" gutter={0}>
                 <Grid.Col span={4} visibleFrom="xs">
                     <Group justify="flex-start">
-                        <Text c="gray" size="sm">
-                            {description}
+                        <Text c="gray" size="sm" tt="uppercase">
+                            {title}
                         </Text>
                     </Group>
                 </Grid.Col>
@@ -49,15 +51,17 @@ export default function HeaderView({description, socials}: Props): React.JSX.Ele
                 <Grid.Col span={4} visibleFrom="xs">
                     <Group gap={4} justify="flex-end">
                         {socials?.filter(v => v?.link).map((v, ind) => (
-                            <ActionIcon
-                                size="xl"
-                                color="gray" variant="subtle"
-                                component="a"
-                                href={v.link}
-                                target="_blank" rel="noopener noreferrer"
-                                key={ind}>
-                                <v.Icon size={20}/>
-                            </ActionIcon>
+                            <Tooltip label={v.label}>
+                                <ActionIcon
+                                    size="xl"
+                                    color="gray" variant="subtle"
+                                    component="a"
+                                    href={v.link}
+                                    target="_blank" rel="noopener noreferrer"
+                                    key={ind}>
+                                    <v.Icon size={20}/>
+                                </ActionIcon>
+                            </Tooltip>
                         ))}
                     </Group>
                 </Grid.Col>
