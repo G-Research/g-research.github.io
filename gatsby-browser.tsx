191ea673c776a47e745171c3a2292448c6da9120
Sofia Sarhiri <143043927+sarhiri@users.noreply.github.com>
2026-09-21 15:45:54 -0500

    Revert "Gatsby react migration"

---
+++ gatsby-browser.tsx
@@ -0,0 +1,12 @@
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/carousel/styles.css';
+
+import * as React from "react";
+import type {GatsbyBrowser} from "gatsby";
+import {MantineProvider} from '@mantine/core';
+import {theme} from "@core/theme";
+
+export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({element}) => {
+    return <MantineProvider theme={theme} defaultColorScheme="auto">{element}</MantineProvider>;
+};
---
+++ src/components/IndexPage/HeroSection/HeroSection.view.tsx
@@ -0,0 +1,60 @@
+import {Container, Text, Button, Flex, Space} from '@mantine/core'
+import * as React from "react"
+import {IconBrandGithub, IconChevronsDown} from '@tabler/icons-react'
+import * as classes from './HeroSection.module.css'
+
+type Props = {
+    onExploreProjectsClick: () => void;
+}
+
+
+export default function HeroSectionView({onExploreProjectsClick}: Props): React.JSX.Element {
+    return <>
+        <Container size='xl' className={classes.inner}>
+            <h1 className={classes.title}>
+                Open-Source Software at{' '}
+                <Text component="span" variant="gradient" gradient={{from: 'blue', to: 'cyan'}} inherit
+                      className={classes.brand}>
+                    G-Research
+                </Text>
+            </h1>
+
+            <Text className={classes.description} c="dimmed">
+                We play an active role in the open-source community. We take responsibility for maintaining projects,
+                contributing to the continual development of essential software and supporting open-source foundations.
+                <Space component="br"/>
+                <Space component="br"/>
+                Our work focuses on data science and machine learning tools, as well as the infrastructure and security
+                to
+                support them.
+            </Text>
+
+            <Flex pt='xl'
+                  align={{base: "stretch", sm: "space-between"}}
+                  direction={{base: 'column', sm: 'row'}}
+                  gap={{base: 'md', sm: 'lg'}}
+            >
+                <Button
+                    size="lg"
+                    variant="gradient"
+                    gradient={{from: 'blue', to: 'cyan'}}
+                    leftSection={<IconChevronsDown size={20}/>}
+                    onClick={onExploreProjectsClick}
+                >
+                    Explore our projects
+                </Button>
+
+                <Button
+                    component="a"
+                    href="https://github.com/G-Research"
+                    target="_blank" rel="noopener noreferrer"
+                    size="lg"
+                    variant="default"
+                    leftSection={<IconBrandGithub size={20}/>}
+                >
+                    GitHub
+                </Button>
+            </Flex>
+        </Container>
+    </>
+}
